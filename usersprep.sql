078f39b9a00354f23bea71de90f21fa21219f291
bwalwyn <bwalwyn@gmail.com>
2020-06-20 21:53:26 +0200

    webusers

--- usersprep.sql
+++ usersprep.sql
@@ -16,3 +16,22 @@ from upya_uganda.users b
 where b.user_upya_id = a.user_upya_id
  and b.user_email != a.user_email
 ;
+
+-- Web USER FIXES
+-- Upya - Malawi
+update core.webusers a
+set webuser_email = b.webuser_email
+	, webuser_username = b.webuser_username
+from upya.webusers b
+where b.webuser_upya_id = a.webuser_upya_id
+ and b.webuser_email != a.webuser_email
+;
+
+--Upya Uganda
+update core.webusers a
+set webuser_email = b.webuser_email
+	, webuser_username = b.webuser_username
+from upya_uganda.webusers b
+where b.webuser_upya_id = a.webuser_upya_id
+ and b.webuser_email != a.webuser_email
+;
