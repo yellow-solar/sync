40b601c0dfb8849e95d7a0df6a4e1ec0b9aa00dd
bwalwyn <bwalwyn@gmail.com>
2020-11-04 09:57:37 +0200

    payment ids

--- update_zohoid.py
+++ update_zohoid.py
@@ -3,18 +3,28 @@ from yellowpgdb import yellowpgdb
 
 accounts = pd.read_csv('IDupdates/account_ids_to_update.csv',dtype = str,)[['account_id','account_external_id','ID']]
 apps = pd.read_csv('IDupdates/app_ids_to_update.csv',dtype = str,)[['application_id','application_external_id','ID']]
+payments = pd.read_csv('IDupdates/payment_ids_to_update.csv',dtype = str,)[['payment_external_id','ID']]
 
 db = yellowpgdb()
 conn = db.connect()
 
-# Accounts
+# Payments
 with conn.cursor() as cursor:
-    for i in accounts.index:
-        print(accounts.loc[i])
-        row = accounts.loc[i]
-        sql = f"update core.accounts set zoho_id = {row['ID']} where account_external_id = '{row['account_external_id']}'"
+    for i in payments.index:
+        print(payments.loc[i])
+        row = payments.loc[i]
+        sql = f"update core.payments set zoho_id = {row['ID']} where payment_external_id = '{row['payment_external_id']}'"
         print(sql)
-        cursor.execute(sql)     
+        cursor.execute(sql)   
+
+# Accounts
+# with conn.cursor() as cursor:
+#     for i in accounts.index:
+#         print(accounts.loc[i])
+#         row = accounts.loc[i]
+#         sql = f"update core.accounts set zoho_id = {row['ID']} where account_external_id = '{row['account_external_id']}'"
+#         print(sql)
+#         cursor.execute(sql)     
 
 # Applications
 # with conn.cursor() as cursor:
